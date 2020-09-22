a06fcc97ba294b97894fca772351e108754fdc6d
francisco-1998 <francmarinc@gmail.com>
2020-09-22 13:18:11 -0500

    Terminada la validacion

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styleUrls: ['./template.component.css']
+  styleUrls: ['./template.component.css'],
 })
 export class TemplateComponent implements OnInit {
+  usuario = {
+    nombre: 'Francisco',
+    apellido: 'Marin',
+    email: 'francmarinc@gmail.com',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  infoFormulario(formtemplate: NgForm) {
+    // console.log(formtemplate);
+    if (formtemplate.invalid) {
+      Object.values(formtemplate.controls).forEach((controls) => {
+        controls.markAllAsTouched();
+      });
+      return;
+    } else {
+      console.log('Datos del formulario');
+      console.log(formtemplate.value);
+    }
   }
-
 }
